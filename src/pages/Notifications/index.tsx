6dbe6ca7f6611739f93bb757086d30e0c9d4d341
Elvis <elvis.e.amancio@gmail.com>
2021-10-20 21:01:53 -0300

    Changes Notifications

--- src/pages/Notifications/index.tsx
+++ src/pages/Notifications/index.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native';
+import IconFeather from 'react-native-vector-icons/Feather';
 import { Header } from '../../components/Header';
-import { Container } from './styles';
+import TrashIcon from '../../assets/trash-icon.svg'
 
-const Notifications: React.FC = () => (
-  <Container>
-    <Header>Notificações</Header>
-  </Container>
-);
+import {
+  Container,
+  ContainerCard,
+  ContainerChat,
+  ImageUser,
+  Content,
+  Name,
+  Message,
+  ContentLeft,
+} from './styles';
 
-export default Notifications;
+export default function Notifications(): JSX.Element {
+  const navigation = useNavigation();
+  return (
+    <Container>
+      <Header>Notifications</Header>
+      <ContainerChat>
+        <ContainerCard>
+          <ContentLeft>
+            <ImageUser source={{ uri: 'https://github.com/elvisea.png' }} />
+            <Content>
+              <Name>Armin Salahuddin</Name>
+              <Message>Confirmação data 01/05/2021</Message>
+            </Content>
+          </ContentLeft>
+          <TouchableOpacity onPress={() => navigation.navigate('Assessment')}>
+            <TrashIcon color="#0A3FA5" />
+          </TouchableOpacity>
+        </ContainerCard>
+      </ContainerChat>
+    </Container>
+  );
+}
